8cb4c56530e9071da365a940a0794f625cf8a394
Tiago <tiago.aguiar@proposify.com>
2021-05-08 14:36:06 +0100

    add: Input component

--- src/components/base/Input.js
+++ src/components/base/Input.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import styled from 'styled-components/macro';
-import Emoji from '../base/Emoji';
 
 const InputWrapper = styled.div`
     height: 48px;
     width: 100%;
     position: relative;
     margin: 0 auto;
+
+    label {
+        display: inline-block;
+        max-width: 100%;
+        margin-bottom: 5px;
+        font-size: 14px;
+        color: ${props => props.theme.colors.default}
+    }
 
     span {
         position: absolute;
@@ -22,23 +29,33 @@ const InputWrapper = styled.div`
 
 
 const StyledInput = styled.input`
-    height: 48px;
+    display: block;
     width: 100%;
-    padding: 0 64px;
-    border-radius: 100px;
-    border: none;
-    font-size: 14px;
-    outline: none;
+    height: 40px;
+    padding: 8.3333336px 14px;
+    font-size: 16px;
+    line-height: 1.3333333;
+    background-color: ${props => props.theme.colors.white};
+    background-image: none;
+    border: 1px solid #bbbbbb;
+    border-radius: 3px;
+    -webkit-box-shadow: inset 0 1px 3px -1px #aaa;
+    box-shadow: inset 0 1px 3px -1px #aaa;
+    -webkit-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+    -o-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+    transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
     color: ${props => props.theme.colors.text};
     &::placeholder {
         color: gray;
-        font-family: ${props => props.theme.fonts.montserrat}
+        font-family: ${props => props.theme.fonts.main}
     }
-`
+`;
 
 const Input = (props) => (
     <InputWrapper>
-        <Emoji symbol="🔎" label="search" />
+        {props.label && 
+            <label for={props.id}>{props.label}</label>
+        }
         <StyledInput {...props}/>
     </InputWrapper>
 )
--- src/stories/Input.stories.js
+++ src/stories/Input.stories.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Input } from '../components/base';
+import { text } from '@storybook/addon-knobs';
+
 
 storiesOf('Base/Input', module)
     .add('Playground', () => {
         return (
-            <Input />
+            <Input label={text('Label', 'Input Label')}/>
         )
     })
