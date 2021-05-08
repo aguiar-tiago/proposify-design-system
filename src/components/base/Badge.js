d8d46f6535a326c822cb154dcde394c74fab9ede
Tiago <tiago.aguiar@proposify.com>
2021-05-08 10:37:27 +0100

    add: badges variations

--- src/components/base/Badge.js
+++ src/components/base/Badge.js
@@ -1,15 +1,44 @@
+import React from 'react';
 import styled from 'styled-components';
 
-const Badge = styled.div`
-    background-color: ${props => props.theme.colors.secondary};
+const StyledBadge = styled.div`
+    background-color: ${props => props.active && props.theme.colors.info};
     font-family: ${props => props.theme.fonts.montserrat};
-    max-width: 100px;
-    color: white;
+    color: ${props => props.active ? props.theme.colors.white : props.theme.colors.info};
     padding: 6px;
-    border-radius: 2px;
+    border-radius: 3px;
+    text-align: center;
+    padding: 5px 15px;
     font-weight: bold;
-    box-shadow: 2px 2px 5px rgba(0,0,0,0.3);
+    box-shadow: ${props => props.active && '1px 1px 1px 0 #bbb'};
+    width: ${props => props.theme.buttonSizes[props.size]}
 
+    span {
+        background-color: ${props => props.active ? props.theme.colors.white : props.theme.colors.info};
+        color: ${props => props.active ? props.theme.colors.info : props.theme.colors.white};
+        margin-left: 3px;
+        height: 26px;
+        width: 26px;
+        border: 1px solid #fff;
+        padding: 7px 0;
+        text-align: center;
+        display: inline-block;
+        min-width: 10px;
+        font-size: 10.5px;
+        font-weight: 500;
+        line-height: 1;
+        vertical-align: middle;
+        white-space: nowrap;
+        text-align: center;
+        border-radius: 25px;
+    }
 `
+
+const Badge = ({active = true, amount, children, size}) => (
+    <StyledBadge active={active} size={size}>
+        {children}
+        {amount && <span>{amount}</span>}
+    </StyledBadge>
+)
  
 export default Badge;
--- src/stories/Badge.stories.js
+++ src/stories/Badge.stories.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { text } from '@storybook/addon-knobs';
+import { text, boolean, number, select } from '@storybook/addon-knobs';
 import { Badge } from '../components/base';
 
 storiesOf('Base/Badge', module)
-    .add('Playground', () => {
+.add('Playground', () => {
+        const sizes = select('Size', ['xxs', 'sm', 'md', 'lg'], 'md');
         return (
-            <Badge>{text('label', '50% OFF')}</Badge>
+            <Badge
+                active={boolean('active', true)}
+                amount={number('amount', 32)}
+                size={sizes}
+            >
+                { text('label', 'Home') }
+            </Badge>
         )
     })
