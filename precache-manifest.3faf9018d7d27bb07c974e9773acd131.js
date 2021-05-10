e01fb5c4d79f09bdd489d0e9b4016065f142f9a2
Tiago <tiago.aguiar@proposify.com>
2021-05-10 13:38:39 +0100

    Updates

---
+++ precache-manifest.3faf9018d7d27bb07c974e9773acd131.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc813ae0a397114806d",
+    "url": "0.7a9fd882.iframe.bundle.js"
+  },
+  {
+    "revision": "a35fe717a0ff63e1ade4",
+    "url": "4.ad067b51.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.ad067b51.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "323bd904b3e817cc01e1",
+    "url": "5.92a8592e.iframe.bundle.js"
+  },
+  {
+    "revision": "1a87126c092a2b35f849",
+    "url": "6.0d844eb4.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.0d844eb4.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d44b1ea6798837bf2c",
+    "url": "7.c6c7f4dd.iframe.bundle.js"
+  },
+  {
+    "revision": "984cf58bb01214f78358e8178d7918fe",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "b2de1b06cc6d1c48b1e2",
+    "url": "main.017694f7.iframe.bundle.js"
+  },
+  {
+    "revision": "14b494ee69d01a97eb73",
+    "url": "runtime~main.4fcf437e.iframe.bundle.js"
+  },
+  {
+    "revision": "0988922e8ed380689ca54855833342c9",
+    "url": "static/media/ProximaNova-Bold-webfont.0988922e.woff"
+  },
+  {
+    "revision": "4d4031d425cd5a78594bf94b583c40f9",
+    "url": "static/media/ProximaNova-Bold-webfont.4d4031d4.eot"
+  },
+  {
+    "revision": "5a8bc5505bfb146cb8fecd2804b116eb",
+    "url": "static/media/ProximaNova-Bold-webfont.5a8bc550.ttf"
+  },
+  {
+    "revision": "5ed65258519fe2c7c00912300061282d",
+    "url": "static/media/ProximaNova-Bold-webfont.5ed65258.woff2"
+  },
+  {
+    "revision": "7e40079e3e5acbb38e921037f548ee34",
+    "url": "static/media/ProximaNova-Bold-webfont.7e40079e.svg"
+  },
+  {
+    "revision": "2f5f4c3da7338d0c4a45e2f1acaa0108",
+    "url": "static/media/ProximaNova-BoldIt-webfont.2f5f4c3d.eot"
+  },
+  {
+    "revision": "79623f3b193ed8a6f9bc3434d2cea0ae",
+    "url": "static/media/ProximaNova-BoldIt-webfont.79623f3b.svg"
+  },
+  {
+    "revision": "a5f812d6f5120eb66c5fc9e11effb78b",
+    "url": "static/media/ProximaNova-BoldIt-webfont.a5f812d6.woff"
+  },
+  {
+    "revision": "ada13774266d2e20cbc7b33519aa1375",
+    "url": "static/media/ProximaNova-BoldIt-webfont.ada13774.woff2"
+  },
+  {
+    "revision": "c96ad85a5092f6ba1b79dca7b3e48bd9",
+    "url": "static/media/ProximaNova-BoldIt-webfont.c96ad85a.ttf"
+  },
+  {
+    "revision": "01593b54d9e1069e75813fbd5b81d2dd",
+    "url": "static/media/ProximaNova-Light-webfont.01593b54.woff"
+  },
+  {
+    "revision": "562e97c39d6aa3f0f4ead06db4873ef3",
+    "url": "static/media/ProximaNova-Light-webfont.562e97c3.ttf"
+  },
+  {
+    "revision": "8b7a2ea3ead03ba763da54c65bc6975c",
+    "url": "static/media/ProximaNova-Light-webfont.8b7a2ea3.woff2"
+  },
+  {
+    "revision": "c077afa46406518f851de1195b3b5493",
+    "url": "static/media/ProximaNova-Light-webfont.c077afa4.eot"
+  },
+  {
+    "revision": "e17a17d7e7fe005f060bfb0bf855aec7",
+    "url": "static/media/ProximaNova-Light-webfont.e17a17d7.svg"
+  },
+  {
+    "revision": "00cca496520dc70ee613e180cd7053ea",
+    "url": "static/media/ProximaNova-LightIt-webfont.00cca496.ttf"
+  },
+  {
+    "revision": "114bb2a894dbd823065ec57ec136b1e1",
+    "url": "static/media/ProximaNova-LightIt-webfont.114bb2a8.woff"
+  },
+  {
+    "revision": "2c6b5e3ac57a4d6c7312a97907f79330",
+    "url": "static/media/ProximaNova-LightIt-webfont.2c6b5e3a.woff2"
+  },
+  {
+    "revision": "68942cb1252757d399264f98807cf76d",
+    "url": "static/media/ProximaNova-LightIt-webfont.68942cb1.eot"
+  },
+  {
+    "revision": "daf98cc6c5195b1c94cf617680c0425c",
+    "url": "static/media/ProximaNova-LightIt-webfont.daf98cc6.svg"
+  },
+  {
+    "revision": "2d636d9395b2da27ce67040250333ca4",
+    "url": "static/media/ProximaNova-Reg-webfont.2d636d93.woff2"
+  },
+  {
+    "revision": "5d0e746af028be8766181f227b3e87d1",
+    "url": "static/media/ProximaNova-Reg-webfont.5d0e746a.woff"
+  },
+  {
+    "revision": "70a537253a0427af6534842b8b2c3b2e",
+    "url": "static/media/ProximaNova-Reg-webfont.70a53725.eot"
+  },
+  {
+    "revision": "ba9eadeb7b4a81dbf118d1f071ea9abe",
+    "url": "static/media/ProximaNova-Reg-webfont.ba9eadeb.svg"
+  },
+  {
+    "revision": "bbb808133b0eba2f0917fbc4e9b88a28",
+    "url": "static/media/ProximaNova-Reg-webfont.bbb80813.ttf"
+  },
+  {
+    "revision": "30aab7faa2661a87fe368cb27a9ee9a1",
+    "url": "static/media/ProximaNova-RegIt-webfont.30aab7fa.ttf"
+  },
+  {
+    "revision": "5d399e5a0390a2732247e32c5293b64b",
+    "url": "static/media/ProximaNova-RegIt-webfont.5d399e5a.woff"
+  },
+  {
+    "revision": "bae3f933ba10973f540094988a2ad267",
+    "url": "static/media/ProximaNova-RegIt-webfont.bae3f933.svg"
+  },
+  {
+    "revision": "c0444813ab1ad53ddaa88361ea613fe1",
+    "url": "static/media/ProximaNova-RegIt-webfont.c0444813.woff2"
+  },
+  {
+    "revision": "e38d21495e5a09beaa29a6594d8e0644",
+    "url": "static/media/ProximaNova-RegIt-webfont.e38d2149.eot"
+  },
+  {
+    "revision": "4422354e6ede961bef726afa1be9522b",
+    "url": "static/media/ProximaNova-Sbold-webfont.4422354e.eot"
+  },
+  {
+    "revision": "6c1b0f2a27368ddc12cdd62d70ba9c7a",
+    "url": "static/media/ProximaNova-Sbold-webfont.6c1b0f2a.svg"
+  },
+  {
+    "revision": "a96ff4477074c6395b7305d2d98fde8e",
+    "url": "static/media/ProximaNova-Sbold-webfont.a96ff447.woff2"
+  },
+  {
+    "revision": "bba11955959ea56cb0c1ae3d3b9b9b2a",
+    "url": "static/media/ProximaNova-Sbold-webfont.bba11955.woff"
+  },
+  {
+    "revision": "fbfe5c8a6f2ad5350380f04fe09ba44f",
+    "url": "static/media/ProximaNova-Sbold-webfont.fbfe5c8a.ttf"
+  },
+  {
+    "revision": "0b53a19e43f5101e3da46c6d6cf1384b",
+    "url": "static/media/ProximaNova-SboldIt-webfont.0b53a19e.woff"
+  },
+  {
+    "revision": "811884524685a5b3a46185d97c306f79",
+    "url": "static/media/ProximaNova-SboldIt-webfont.81188452.ttf"
+  },
+  {
+    "revision": "bc53d6e72b4d7e67f51986360c557d38",
+    "url": "static/media/ProximaNova-SboldIt-webfont.bc53d6e7.svg"
+  },
+  {
+    "revision": "d0f1c94486938415a55de46c971db36e",
+    "url": "static/media/ProximaNova-SboldIt-webfont.d0f1c944.eot"
+  },
+  {
+    "revision": "d4f46bdf49e0865d4f98c7e60aacb584",
+    "url": "static/media/ProximaNova-SboldIt-webfont.d4f46bdf.woff2"
+  },
+  {
+    "revision": "3ab5765bef19fc1aa206c29cda460a52",
+    "url": "static/media/ProximaNova-Thin-webfont.3ab5765b.woff2"
+  },
+  {
+    "revision": "87f04f5560d3bfb64e6ec4c725e893a1",
+    "url": "static/media/ProximaNova-Thin-webfont.87f04f55.svg"
+  },
+  {
+    "revision": "964230c9237c839c06e7d44f634c2c64",
+    "url": "static/media/ProximaNova-Thin-webfont.964230c9.eot"
+  },
+  {
+    "revision": "cdd2aa79aa4f61fa4298595d7a3b04ce",
+    "url": "static/media/ProximaNova-Thin-webfont.cdd2aa79.woff"
+  },
+  {
+    "revision": "e5a1d9064867b89e329a7a74c9962397",
+    "url": "static/media/ProximaNova-Thin-webfont.e5a1d906.ttf"
+  },
+  {
+    "revision": "5efd1aa1232b0afc6c7b5cd91ac0d60d",
+    "url": "static/media/ProximaNova-ThinIt-webfont.5efd1aa1.svg"
+  },
+  {
+    "revision": "67ef0b88e3d7a667feb97a9bf8f08900",
+    "url": "static/media/ProximaNova-ThinIt-webfont.67ef0b88.ttf"
+  },
+  {
+    "revision": "701d1a862417e00ba069b2959b1ff11c",
+    "url": "static/media/ProximaNova-ThinIt-webfont.701d1a86.woff"
+  },
+  {
+    "revision": "9b591a3e57ab2119aac3eb1d7d76f5d9",
+    "url": "static/media/ProximaNova-ThinIt-webfont.9b591a3e.eot"
+  },
+  {
+    "revision": "e230591cc01e3af73f1dee3445caf28d",
+    "url": "static/media/ProximaNova-ThinIt-webfont.e230591c.woff2"
+  },
+  {
+    "revision": "6e8ceb36f4569679119e",
+    "url": "vendors~main.f607f4c1.iframe.bundle.js"
+  },
+  {
+    "revision": "7bbf1817f8360b23068317a2e58a68b1",
+    "url": "vendors~main.f607f4c1.iframe.bundle.js.LICENSE.txt"
+  }
+]);
